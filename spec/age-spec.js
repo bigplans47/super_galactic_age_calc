85c092727ede0503731b5d92b8fcff59ae4cf485
Dan Kiss <flowfast47@gmail.com>
2018-03-16 11:55:08 -0700

    add passing specs to get difference of seconds in dates and calculate age on mercury

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -3,9 +3,21 @@ import { Calculator } from './../src/age_logic';
 describe('Calculator', function() {
 
   it('Should take a person’s age in years and convert it into seconds.', function() {
-    let sample_human = new Calculator(100);
+    let sample_human = new Calculator(10);
     sample_human.func_age_in_seconds();
-    expect(sample_human.age_in_seconds).toEqual(3153600000)
+    expect(sample_human.age_in_seconds).toEqual(315360000)
   });
+
+  it('Should take two dates and determine the difference, in seconds, between the two. (This way a user can enter a birthdate and the application can compare the birthdate against the current time to come up with an accurate age, this is more accurate as it adjusts for leap years))', function() {
+    let sample_human = new Calculator(10);
+    sample_human.func_amount_of_seconds_lived(2008, 3, 16);
+    expect(sample_human.age_in_seconds_precise).toEqual(315532800)
+  });
+
+  it('Should Return the age of a human in Mercury years. (A Mercury year is .24 Earth years.))', function() {
+    let sample_human = new Calculator(10);
+    expect(sample_human.func_age_on_mercury()).toEqual(10/.24)
+  });
+
 
 });
--- src/age_logic.js
+++ src/age_logic.js
@@ -2,10 +2,25 @@ export class Calculator {
   constructor(age_in_years) {
     this.age = age_in_years;
     this.age_in_seconds = null;
+    this.birthdate = null;
+    this.date_today = null;
+    this.age_in_seconds_precise = null;
+    this.age_on_mercury = null;
   }
 
   func_age_in_seconds() {
     this.age_in_seconds = this.age*365*24*60*60;
+  }
+
+  func_amount_of_seconds_lived(year, month, day) {
+    this.date_today = new Date(2018, 2, 16);
+    this.birthdate = new Date(year, month-1, day)
+    this.age_in_seconds_precise = (this.date_today - this.birthdate)/1000
+  }
+
+  func_age_on_mercury(){
+    this.age_on_mercury = this.age/.24
+    return this.age_on_mercury
   }
 
 }

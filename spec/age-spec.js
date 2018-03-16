1b10a876c9b56c78610ecdcced0fa8004a7f1b79
Dan Kiss <flowfast47@gmail.com>
2018-03-16 12:24:36 -0700

    add ages for more planets and tests

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -4,8 +4,9 @@ describe('Calculator', function() {
 
   it('Should take a person’s age in years and convert it into seconds.', function() {
     let sample_human = new Calculator(10);
-    sample_human.func_age_in_seconds();
-    expect(sample_human.age_in_seconds).toEqual(315360000)
+    const age = sample_human.func_age_in_seconds();
+    console.log(age)
+    expect(age).toEqual(315360000)
   });
 
   it('Should take two dates and determine the difference, in seconds, between the two. (This way a user can enter a birthdate and the application can compare the birthdate against the current time to come up with an accurate age, this is more accurate as it adjusts for leap years))', function() {
@@ -18,6 +19,23 @@ describe('Calculator', function() {
     let sample_human = new Calculator(10);
     expect(sample_human.func_age_on_mercury()).toEqual(10/.24)
   });
+
+  it('Should Return the age of a human in Venus years. (A Venus year is .62 Earth years.)', function() {
+    let sample_human = new Calculator(10);
+    expect(sample_human.func_age_on_venus()).toEqual(10/.62)
+  });
+
+  it('Return the age of a human in Mars years. (A Mars year is 1.88 Earth years.)', function() {
+    let sample_human = new Calculator(10);
+    console.log(sample_human)
+    expect(sample_human.func_age_on_mars()).toEqual(10/1.88)
+  });
+// func_age_on_jupiter()
+it('Return the age of a human in Jupiter years. (A Jupiter year is 11.86 Earth years.)', function() {
+  let sample_human = new Calculator(10);
+  console.log(sample_human)
+  expect(sample_human.func_age_on_jupiter()).toEqual(10/11.86)
+});
 
 
 });
--- src/age_logic.js
+++ src/age_logic.js
@@ -6,21 +6,42 @@ export class Calculator {
     this.date_today = null;
     this.age_in_seconds_precise = null;
     this.age_on_mercury = null;
+    this.age_on_venus = null;
+    this.age_on_mars = null;
+    this.age_on_jupiter = null;
+
   }
 
   func_age_in_seconds() {
     this.age_in_seconds = this.age*365*24*60*60;
+    return this.age_in_seconds;
   }
 
   func_amount_of_seconds_lived(year, month, day) {
     this.date_today = new Date(2018, 2, 16);
-    this.birthdate = new Date(year, month-1, day)
-    this.age_in_seconds_precise = (this.date_today - this.birthdate)/1000
+    this.birthdate = new Date(year, month-1, day);
+    this.age_in_seconds_precise = (this.date_today - this.birthdate)/1000;
+    return this.age_in_seconds_precise;
   }
 
   func_age_on_mercury(){
-    this.age_on_mercury = this.age/.24
-    return this.age_on_mercury
+    this.age_on_mercury = this.age/.24;
+    return this.age_on_mercury;
+  }
+
+  func_age_on_venus(){
+    this.age_on_venus = this.age/.62;
+    return this.age_on_venus;
+  }
+
+  func_age_on_mars(){
+    this.age_on_mars = this.age/1.88;
+    return this.age_on_mars;
+  }
+
+  func_age_on_jupiter(){
+    this.age_on_jupiter = this.age/11.86;
+    return this.age_on_jupiter;
   }
 
 }

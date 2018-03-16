0a27a07bc6ba11fc15e83c4910a677048de82459
Dan Kiss <flowfast47@gmail.com>
2018-03-16 13:43:41 -0700

    add passing specs, met all business logic needs

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -4,9 +4,9 @@ describe('Calculator', function() {
 
   it('Should take a person’s age in years and convert it into seconds.', function() {
     let sample_human = new Calculator(10);
-    const age = sample_human.func_age_in_seconds();
-    console.log(age)
-    expect(age).toEqual(315360000)
+    const AGE = sample_human.func_age_in_seconds();
+    console.log(AGE)
+    expect(AGE).toEqual(315360000)
   });
 
   it('Should take two dates and determine the difference, in seconds, between the two. (This way a user can enter a birthdate and the application can compare the birthdate against the current time to come up with an accurate age, this is more accurate as it adjusts for leap years))', function() {
@@ -30,12 +30,32 @@ describe('Calculator', function() {
     console.log(sample_human)
     expect(sample_human.func_age_on_mars()).toEqual(10/1.88)
   });
-// func_age_on_jupiter()
-it('Return the age of a human in Jupiter years. (A Jupiter year is 11.86 Earth years.)', function() {
-  let sample_human = new Calculator(10);
-  console.log(sample_human)
-  expect(sample_human.func_age_on_jupiter()).toEqual(10/11.86)
-});
+
+  it('Return the age of a human in Jupiter years. (A Jupiter year is 11.86 Earth years.)', function() {
+    let sample_human = new Calculator(10);
+    console.log(sample_human)
+    expect(sample_human.func_age_on_jupiter()).toEqual(10/11.86)
+  });
+
+  it('Determine how many years a user has left to live on each planet… (yikes!) To do this, the application will have to calculate the user’s life expectancy. You can determine average life expectancy as you see fit. A simple way to do this would be to have the user input the average life expectancy for their demographic. A more involved way to do this would be to collect other information from the user (e.g. lifestyle, country of residence, activity level, etc.) to determine their life expectancy. Assuming human lifespan is 79 years, if exceeded a person is estimated to life an additional one more year earth year', function() {
+    let sample_human = new Calculator(10);
+    expect(sample_human.func_time_left()).toEqual([69, 69/.24, 69/.62, 69/1.88, 69/11.86]);
+  });
+
+  it('Part 2 - Determine how many years a user has left to live on each planet…  Assuming human lifespan is 79 years, if exceeded a person is estimated to life an additional one more year earth year, this text assumes the person is 85 years old on earth', function() {
+    let sample_human = new Calculator(85);
+    expect(sample_human.func_time_left()).toEqual([1, 1/.24, 1/.62, 1/1.88, 1/11.86]);
+  });
+
+  it('If a user has already surpassed the average life expectancy, return the number of years they have lived past the life expectancy.', function() {
+    let sample_human = new Calculator(85);
+    console.log(sample_human)
+    expect(sample_human.func_years_exceed_life_expectancy()).toEqual('Number of years lived past the life expectancy: '+[6, 6/.24, 6/.62, 6/1.88, 6/11.86])
+    // expect(sample_human.func_time_left()).toEqual([1, 1/.24, 1/.62, 1/1.88, 1/11.86]);
+  });
+
+
+
 
 
 });
--- src/age_logic.js
+++ src/age_logic.js
@@ -9,7 +9,17 @@ export class Calculator {
     this.age_on_venus = null;
     this.age_on_mars = null;
     this.age_on_jupiter = null;
-
+    this.life_expectancy = 79;
+    this.time_left = null;
+    this.time_left_mercury = null;
+    this.time_left_venus = null;
+    this.time_left_mars = null;
+    this.time_left_jupiter = null;
+    this.years_exceed_life_expectancy = null;
+    this.years_exceed_life_expectancy_mercury = null;
+    this.years_exceed_life_expectancy_venus = null;
+    this.years_exceed_life_expectancy_mars = null;
+    this.years_exceed_life_expectancy_jupiter = null;
   }
 
   func_age_in_seconds() {
@@ -42,6 +52,38 @@ export class Calculator {
   func_age_on_jupiter(){
     this.age_on_jupiter = this.age/11.86;
     return this.age_on_jupiter;
+  }
+
+  func_time_left(){
+    if (this.life_expectancy-this.age > 0) {
+      // you have x years left
+      this.time_left = this.life_expectancy-this.age;
+      this.time_left_mercury = this.time_left/.24
+      this.time_left_venus = this.time_left/.62;
+      this.time_left_mars = this.time_left/1.88;
+      this.time_left_jupiter = this.time_left/11.86;
+      return [this.time_left, this.time_left_mercury, this.time_left_venus, this.time_left_mars, this.time_left_jupiter];
+  } else {
+      this.time_left = 1;
+      this.time_left_mercury = this.time_left/.24;
+      this.time_left_venus = this.time_left/.62;
+      this.time_left_mars = this.time_left/1.88;
+      this.time_left_jupiter = this.time_left/11.86;
+      return [this.time_left, this.time_left_mercury, this.time_left_venus, this.time_left_mars, this.time_left_jupiter];
+    }
+  }
+
+  func_years_exceed_life_expectancy(){
+    this.years_exceed_life_expectancy = this.age - this.life_expectancy
+    if (this.years_exceed_life_expectancy>0){
+      this.years_exceed_life_expectancy_mercury = this.years_exceed_life_expectancy/.24;
+      this.years_exceed_life_expectancy_venus = this.years_exceed_life_expectancy/.62;
+      this.years_exceed_life_expectancy_mars = this.years_exceed_life_expectancy/1.88;
+      this.years_exceed_life_expectancy_jupiter = this.years_exceed_life_expectancy/11.86;
+      return 'Number of years lived past the life expectancy: '+[this.years_exceed_life_expectancy, this.years_exceed_life_expectancy_mercury, this.years_exceed_life_expectancy_venus, this.years_exceed_life_expectancy_mars, this.years_exceed_life_expectancy_jupiter];
+  } else {
+      return 'Have not exceed life expectancy'
+    }
   }
 
 }
